f89449e9da435d77d915c0f3b9cd7c44bf2b012f
Tetiana Parashchenko <tetiana.parashchenko@boldcommerce.com>
2024-06-21 13:07:14 -0500

    First navigation test

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+This is Cypress test project.
--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: '74mjo4',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://main-bvxea6i-f2pqf7r2ifebo.ca-1.platformsh.site/',
   },
-});
+})
