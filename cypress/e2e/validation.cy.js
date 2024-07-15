e37f5c29bc6c455277be4e8e222d522db0ce5644
Tetiana Parashchenko <tetiana.parashchenko@boldcommerce.com>
2024-07-15 12:32:19 -0500

    Switch repo

--- cypress/e2e/validation.cy.js
+++ cypress/e2e/validation.cy.js
@@ -2,4 +2,5 @@ describe('Magento 2 stores tests', () => {
   it('Add product to cart', () => {
     cy.visit('/fusion-backpack.html')
   })
+  cy.url().should('include', 'fusion-backpack.html');
 })
